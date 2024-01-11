d559977a5a6a70c7a0c2d7056fd93b2063a271bc
ChoongLim <bcl0206@naver.com>
2024-01-11 20:28:51 +0900

    design: 레이아웃 css적용

--- src/app/_component/SideBar.tsx
+++ src/app/_component/SideBar.tsx
@@ -4,6 +4,7 @@ import { SIDE_BAR_LIST } from '@/constants/sideBarList'
 import React from 'react'
 import styles from './SideBar.module.scss'
 import { useRouter } from 'next/navigation'
+import Image from 'next/image'
 
 const SideBar = () => {
   const router = useRouter()
@@ -14,6 +15,9 @@ const SideBar = () => {
 
   return (
     <nav className={styles.container}>
+      <div className={styles.logoWrapper}>
+        <Image src="/hhb.png" alt="hi" width={80} height={80} />
+      </div>
       <ul className={styles.categoryWrapper}>
         {SIDE_BAR_LIST.map(({ id, category, code, children }) => {
           return (
--- src/app/toy-project/sns/layout.tsx
+++ src/app/toy-project/sns/layout.tsx
@@ -1,6 +1,7 @@
 import React, { ReactNode } from 'react'
 import styles from './Layout.module.scss'
 import Nav from './_component/nav'
+import Image from 'next/image'
 
 interface Props {
   children: ReactNode
@@ -10,6 +11,9 @@ const Layout = ({ children }: Props) => {
   return (
     <main className={styles.mainContainer}>
       <section className={styles.leftWrapper}>
+        <div className={styles.logoWrapper}>
+          <Image src="/hhb.png" alt="logo" width={80} height={80} />
+        </div>
         <Nav />
       </section>
       <section className={styles.rightWrapper}>
